37bded2d9f83ff04f9df5644d8865f7d724ee662
Lachlan Ridley <lachlan@sharesies.co.nz>
2024-12-14 16:10:41 +1300

    Remove unneeded function

--- day13.py
+++ day13.py
@@ -42,29 +42,6 @@ for line1, line2, line3, _ in grouper(4, puzzle.splitlines()):
     games.append(Game(button_a, button_b, prize))
 
 
-def get_button_presses_that_will_reach_target(
-    game: Game, big_prizes: bool = False
-) -> set[(int, int)]:
-    winning_combinations = set()
-    prize_x = game.prize.x
-    prize_y = game.prize.y
-    if big_prizes:
-        prize_x += 10000000000000
-        prize_y += 10000000000000
-    for split in range(prize_x + 1):
-        first_half = split
-        second_half = game.prize.x - split
-        a_presses = first_half / game.button_a.x
-        b_presses = second_half / game.button_b.x
-        y_reached = a_presses * game.button_a.y + b_presses * game.button_b.y
-        if not big_prizes and a_presses > 100 and b_presses > 100:
-            # when doing regular small prizes, presses need to be under 100 for each button
-            continue
-        if a_presses.is_integer() and b_presses.is_integer() and y_reached == prize_y:
-            winning_combinations.add((int(a_presses), int(b_presses)))
-    return winning_combinations
-
-
 def solve_with_urgh_mathematics(
     game: Game, big_prizes: bool = False
 ) -> tuple[int, int] | None:
